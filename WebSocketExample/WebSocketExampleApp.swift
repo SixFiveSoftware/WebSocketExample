ef94a4b0533b95adda9bb864f38b6e01a0e16906
BJ Miller <bj@sixfivesoftware.com>
2024-01-12 20:04:24 -0500

    Initial Commit

---
+++ WebSocketExample/WebSocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketExampleApp.swift
+//  WebSocketExample
+//
+//  Created by BJ Miller on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
