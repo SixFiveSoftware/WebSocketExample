211f75e68b8aee3d221ab986692e993e8111d36d
BJ Miller <bj@sixfivesoftware.com>
2024-01-30 13:44:17 -0500

    Get auto-scroll working in scroll view

---
+++ WebSocketExample/ChatMessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatMessagesView.swift
+//  WebSocketExample
+//
+//  Created by BJ Miller on 1/30/24.
+//
+
+import SwiftUI
+
+struct ChatMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatMessagesView()
+}
